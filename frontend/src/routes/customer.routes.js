f115d0d58e0d806abb513d535bdaa073e221f3bb
Rajtamakuwala3 <98575151+Rajtamakuwala3@users.noreply.github.com>
2025-06-29 18:46:11 +0530

    Project cleaning deleted backend code from frontend

--- frontend/src/routes/customer.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { customerHandler } from "../controllers/customer.controller.js";
-
-const router = Router();
-
-router.route("/create").post(verifyJWT, customerHandler);
-
-export default router;
